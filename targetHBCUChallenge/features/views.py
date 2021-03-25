409675474245b7764014cd23dd12f9530fa6a80b
paulAriri <ariripaulcoc@gmail.com>
2021-03-25 02:41:04 -0500

    Testing Http Responses

--- targetHBCUChallenge/features/views.py
+++ targetHBCUChallenge/features/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home(request):
+    return HttpResponse('Home Page')
+
+def trivia(request):
+    return HttpResponse('Trivia Page')
+
+def budget(request):
+    return HttpResponse('Budget Page')
