c12366b4d0bf864df5985d3c576c985678c3c11e
paulAriri <ariripaulcoc@gmail.com>
2021-03-31 03:15:20 -0500

    completed design of functional webpages and models

--- targetHBCUChallenge/features/urls.py
+++ targetHBCUChallenge/features/urls.py
@@ -3,7 +3,13 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home),
-    path('trivia/', views.trivia),
-    path('budget/', views.budget),
+    path('', views.home, name="home"),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name="register"),
+    path('trivia/', views.trivia, name="trivia"),
+    path('budget/', views.budget, name="budget"),
+    path('budgetlist1/', views.budgetlist1, name="budget_list_one"),
+    path('budgetlist2/', views.budgetlist2, name="budget_list_two"),
+    path('budgetlist3/', views.budgetlist3, name="budget_list_three"),
+    path('triviarules/', views.trivia_rules, name="trivia_rules"),
 ]
--- targetHBCUChallenge/features/views.py
+++ targetHBCUChallenge/features/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
+from .models import *
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def home(request):
     return render(request, 'features/home.html')
 
+def login(request):
+    return render(request, 'features/login.html')
+
+def register(request):
+    return render(request, 'features/register.html')
+
+def trivia_rules(request):
+    return render(request, 'features/trivia_rules.html')
+
+def budgetlist1(request):
+    return render(request, 'features/budgetlist.html')
+
+def budgetlist2(request):
+    return render(request, 'features/budgetlist2.html')
+
+def budgetlist3(request):
+    return render(request, 'features/budgetlist3.html')
+
 def trivia(request):
-    return HttpResponse('Trivia Page')
+    return render(request, 'features/trivia.html')
 
 def budget(request):
-    return HttpResponse('Budget Page')
+    return render(request, 'features/budget.html')
